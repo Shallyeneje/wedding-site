1716e1faa78c9b679bebd5df24b834b4c905a52e
Shallyeneje <Shalomenje2@gmail.com>
2026-05-01 13:25:42 +0100

    payment

---
+++ app/payments/[ref]/page.tsx
@@ -0,0 +1,10 @@
+import PaymentConfirmation from '@/components/modules/payments/payment-confirmation'
+import React from 'react'
+
+const PaymentConfirmationPage = () => {
+  return (
+    <PaymentConfirmation />
+  )
+}
+
+export default PaymentConfirmationPage
---
+++ app/payments/page.tsx
@@ -0,0 +1,12 @@
+import PaymentContainer from '@/components/modules/payments/container'
+import React from 'react'
+
+export const dynamic = 'force-dynamic'
+
+const PaymentPage = () => {
+  return (
+    <PaymentContainer />
+  )
+}
+
+export default PaymentPage
